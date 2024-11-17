49f42f4b6ea8551f6d9b3a41b85e48f70e85df31
2 1 <InotitoKokoro@gmail.com>
2024-11-17 16:02:17 +0900

    さて、競合しそう

---
+++ webapp/main.js
@@ -0,0 +1,10 @@
+/**
+ * @typedef {{"type":string,"tags":string[],"name":string,"amount":number,"fingerprint":string,"isCraftable":boolean,"nbt":object,"displayName":string}} rawMEElement
+ */
+class MEElement {
+    /**@param {rawMEElement} rawData */
+    constructor(rawData) {
+
+    }
+}
+
