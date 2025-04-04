2bc5bed86759a2fd3f0a4f9802c26d530f328eb3
manmen2414 <inotitokokoro@gmail.com>
2025-04-04 15:53:43 +0900

    ポップアップの改善・利用可能に

--- webapp/util/popup.js
+++ webapp/util/popup.js
@@ -1,63 +1,48 @@
-
 class Popup {
-    static showing = null;
-    static HTMLTemplate =
-        `
-<div class="popup-title">
-    <button style="color: black;" onclick="Popup.showing.close(false)">×</button>
-    !!!title!!!
-</div>
-<div class="popup-text">
+  static showing = null;
+  static HTMLTemplate = `
+<div class="popup-message">
+    <button class="popup-delete" onclick="Popup.showing.close(false)">×</button>
     !!!html!!!
 </div>
-`
-    /**
-     * @param {string} title 
-     * @param {string} innerHTML 
-     */
-    constructor(title = "Popup", innerHTML = "") {
-        /**@type {string} */
-        this.title = title;
-        /**@type {string} */
-        this.innerHTML = innerHTML;
-        /**@type {function} */
-        this.callback = () => { };
-    }
+`;
+  /**
+   * @param {string} innerHTML
+   */
+  constructor(innerHTML = "Popup") {
+    /**@type {string} */
+    this.innerHTML = innerHTML;
+    /**@type {function} */
+    this.callback = () => {};
+  }
+  /**
+   * @param {string} innerHTML
+   */
+  setInnerHTML(innerHTML) {
+    this.innerHTML = innerHTML;
+    return this;
+  }
 
-    /**
-     * @param {string} title 
-     */
-    setTitle(title) {
-        this.title = title;
-        return this;
+  show(callback) {
+    if (!!Popup.showing) {
+      callback(false);
+      return;
     }
+    const popup = document.querySelector("#popup");
+    popup.innerHTML = Popup.HTMLTemplate.replace("!!!html!!!", this.innerHTML);
+    popup.style.display = "block";
+    popup.style.left = `${
+      (document.body.clientWidth - popup.clientWidth) / 2
+    }px`;
+    Popup.showing = this;
+    this.callback = callback;
+  }
 
-    /**
-     * @param {string} innerHTML
-     */
-    setInnerHTML(innerHTML) {
-        this.innerHTML = innerHTML;
-        return this;
-    }
-
-    show(callback) {
-        if (!!Popup.showing) callback(false);
-        const popup = document.querySelector("#popup");
-        popup.innerHTML = Popup.HTMLTemplate
-            .replace("!!!title!!!", this.title)
-            .replace("!!!html!!!", this.innerHTML);
-        popup.style.display = "block";
-        document.querySelector("#main-document").inert = true;
-        Popup.showing = this;
-        this.callback = callback;
-    }
-
-    close(sucessed = true) {
-        if (!Popup.showing) return false;
-        const popup = document.querySelector("#popup");
-        popup.style.display = "none";
-        document.querySelector("#main-document").inert = false;
-        Popup.showing = null;
-        return this.callback(sucessed);
-    }
+  close(sucessed = true) {
+    if (!Popup.showing) return false;
+    const popup = document.querySelector("#popup");
+    popup.style.display = "none";
+    Popup.showing = null;
+    return this.callback(sucessed);
+  }
 }
